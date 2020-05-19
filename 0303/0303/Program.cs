be135fb71da54ac73327e710b72504baf9c9078e
陳姿伶 <sophiechen@chenzilingde-MacBook-Pro.local>
2020-05-19 20:14:41 +0800

    initialization

---
+++ 0303/0303/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _0303
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i >= 0)
+            {
+                if (((i + 1) * (i+1) -i * i)> 169)
+                {
+                    break;
+                }
+                i++;
+            }
+
+            int[] arr = new int[i];
+            int j = 0;
+            for (; j <= i; j++)
+            {
+                arr[j] = j * j;
+            }
+            int x, y;
+            for(x = 0; x <= i; x++)
+            {
+                for(y= 0; y <= i; y++)
+                {
+                    if (arr[y] - arr[x] == 168)
+                    {
+                        break;
+                    }
+                }
+            }
+            int a = arr[i] - 100;
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* Console.WriteLine("n:");*/
+           
+            for (int l = 1; ;l++) {
+                int n = Convert.ToInt32(Console.ReadLine());
+                if (n == 0) { break; }
+                int[] array1 = new int[n];
+                int k = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    /*Console.Write("h:");*/
+                    int h = Convert.ToInt32(Console.ReadLine());
+                    array1[i] = h;
+                    k = k + array1[i];
+                }
+                int x = k / n;
+                int y = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (array1[j] - x >= 0)
+                    {
+                        y = y + (array1[j] - x);
+                    }
+                    else
+                    {
+                        y = y + (x - array1[j]);
+                    }
+                }
+                Console.WriteLine("Set #" + l);
+                Console.WriteLine("The minimum number of moves is " + y/2+".");
+            }
+        }
+    }
+}
